ded0f466701a00f3a333ae17a255117b3d3c86c7
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-07-11 09:44:18 +0900

    💄 全体のスタイルを調整

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Sidebar from "../components/sidebar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="app">
+      <Sidebar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
